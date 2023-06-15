8f860f6d672ccf1db236dbab7b8aed6453c6b662
Alex <adrewno14@gmail.com>
2023-06-15 14:26:14 -0500

    Added some icons and updated ui a bit to make it look nicer :)

---
+++ app/components/icons/Play.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { TouchableOpacity, Image, StyleSheet } from 'react-native'
+import PlayImg from '../../assets/play.png'
+
+type PlayProps = {
+    onPress: any
+}
+
+export function PlayButton({ onPress }: PlayProps) {
+    return (
+        <TouchableOpacity style={styles.container} onPress={onPress}>
+            <Image style={styles.img} source={PlayImg} />
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: 30,
+        height: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    img: {
+        width: 20,
+        height: 20,
+        tintColor: 'white',
+    },
+})
